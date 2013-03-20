4d1df4df1a8392429d4c31585eef7919d4b26e79
edwardlando <edwardlando8@gmail.com>
2013-03-20 16:23:52 -0400

    push to heroku

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pavelgraham::Application.config.secret_token = 'f133e656fad54b9c9977436d9019c3fec7bf971b442edf453bb0f2dbe9b0608742f71756f4bb343598e1802b358fbae354cd0af23e6a2fc2f7fd039d4dbcba52'
